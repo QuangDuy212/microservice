43d2df574a33da647a8c7a2757a6ac059c7d4aee
QuangDuy101 <duy2k4ml1234@gmail.com>
2024-10-10 22:28:21 +0700

    add notification and kafka

--- employeeservice/src/main/java/com/quangduy/employeeservice/command/controller/EmployeeCommandController.java
+++ employeeservice/src/main/java/com/quangduy/employeeservice/command/controller/EmployeeCommandController.java
@@ -3,6 +3,8 @@ package com.quangduy.employeeservice.command.controller;
 import java.util.UUID;
 
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,18 +13,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.quangduy.employeeservice.command.command.CreateEmployeeCommand;
 import com.quangduy.employeeservice.command.command.DeleteEmployeeCommand;
 import com.quangduy.employeeservice.command.command.UpdateEmployeeCommand;
 import com.quangduy.employeeservice.command.model.EmployeeRequestModel;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.support.MessageBuilder;
 
 @RestController
 @RequestMapping("/api/v1/employees")
+@EnableBinding(Source.class)
 public class EmployeeCommandController {
     private CommandGateway commandGateway;
+    private MessageChannel output;
 
-    public EmployeeCommandController(CommandGateway commandGateway) {
+    public EmployeeCommandController(CommandGateway commandGateway, MessageChannel output) {
         this.commandGateway = commandGateway;
+        this.output = output;
     }
 
     @PostMapping
@@ -54,5 +63,18 @@ public class EmployeeCommandController {
         DeleteEmployeeCommand command = new DeleteEmployeeCommand(employeeId);
         commandGateway.sendAndWait(command);
         return "Deleted Employee";
+    }
+
+    @PostMapping("/sendMessage")
+    public void SendMessage(@RequestBody String message) {
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(message);
+            output.send(MessageBuilder.withPayload(json).build());
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
